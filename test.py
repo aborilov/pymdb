18d46f2fe14f409e4c35c835ea1c5311f59cccff
Aborilov Pavel <aborilov@gmail.com>
2015-08-10 10:48:44 +0300

    devide into classes

--- test.py
+++ test.py
@@ -21,12 +21,94 @@ form = logging.Formatter(
 handler.setFormatter(form)
 logger.addHandler(handler)
 
-print('hello work')
-logger.debug('Hello')
+
+def pretty(data):
+    return ' '.join(["0x%0.2X"%ord(c) for c in data])
+
+def encode(command, data=''):
+    return chr(len(data)+2) + '\x88' + command + data
+
+def log_result(f):
+
+    @defer.inlineCallbacks
+    def pretty_log(self, *args, **kwargs):
+        clazz = ''
+        if self.__class__:
+            clazz = self.__class__.__name__
+        logger.debug("{}.{} ->".format(clazz, f.func_name))
+        result = yield f(self, *args, **kwargs)
+        str_data = pretty(result)
+        logger.debug("{}.{} <- {}".format(clazz, f.func_name, str_data))
+        defer.returnValue(result)
+    return pretty_log
+
+
+class Changer(object):
+
+    def __init__(self, proto):
+        self.proto = proto
+
+    commands = {
+        'reset': '\x08',
+        'poll': '\x0B',
+        'coin_type': '\x0C',
+        'dispense': '\x0D'
+    }
+
+    @log_result
+    def reset(self):
+        request = encode(self.commands['reset'])
+        return self.proto.call(request)
+
+    @log_result
+    def poll(self):
+        request = encode(self.commands['poll'])
+        return self.proto.call(request)
+
+    @log_result
+    def coin_type(self):
+        request = encode(self.commands['coin_type'], "\xFF\xFF\xFF\xFF")
+        return self.proto.call(request)
+
+
+class BillValidator(object):
+
+    def __init__(self, proto):
+        self.proto = proto
+
+    commands = {
+        'reset': '\x30',
+        'poll': '\x33',
+        'bill_type': '\x34',
+        'escrow': '\x35',
+        'stacker': '\x36'
+    }
+
+    @log_result
+    def reset(self):
+        request = encode(self.commands['reset'])
+        return self.proto.call(request)
+
+    @log_result
+    def poll(self):
+        request = encode(self.commands['poll'])
+        return self.proto.call(request)
+
+    @log_result
+    def bill_type(self):
+        request = encode(self.commands['bill_type'], '\xFF\xFF\xFF\xFF')
+        return self.proto.call(request)
+
+    @log_result
+    def escrow(self):
+        request = encode(self.commands['escrow'], '\x00')
+        return self.proto.call(request)
+
+
 
 class MDB(LineReceiver):
 
-    sent = False
+    timeout = 0.3
 
     def __init__(self):
         self.req = None
@@ -35,125 +117,77 @@ class MDB(LineReceiver):
         self.data = ''
 
     def connectionMade(self):
-        print "Connected"
-        print "writing"
-        self.transport.write('\x02\x85\x0A')
-        # self.transport.write('\x02\x88\x08')
-        # self.transport.write('\x02\x88\x30')
-        # self.transport.write('\x02\x88\x0A')
-        # self.transport.write('\x02\x88\x0B')
-        # self.transport.write('\x02\x88\x0C\xFF\xFF\xFF\xFF')
+        logger.debug("Connected")
 
     def rawDataReceived(self, data):
-        str_data = ' '.join(["0x%0.2X"%ord(c) for c in data])
-        # print "data received: {}".format(str_data)
-        if self.req:
-            self.data = self.data + data
-            if self.req[1]=='\x88' and len(self.data)<2:
-                # print 'data too small: {}'.format(data)
-                return
-            str_data = ' '.join(["0x%0.2X"%ord(c) for c in self.data])
-            self.defer.callback(str_data)
-        # str_data = ' '.join(["0x%0.2X"%ord(c) for c in data])
-        # print "data received: {}".format(str_data)
-
-        # if not self.sent:
-            # print "coin type"
-            # self.transport.write('\x06\x88\x0C\xFF\xFF\xFF\xFF')
-            # self.transport.write('\x03\x88\x0D\x01')
-            # self.sent = True
-        # else:
-        # self.transport.write('\x02\x88\x0B')
-        # self.transport.write('\x02\x88\x33')
-
-    def payout(self):
-        self.transport.write('\x03\x88\x0D\x12')
-        # self.transport.write('\x03\x88\x35\x00')
-
-    def payin(self):
-        self.transport.write('\x06\x88\x0C\xFF\xFF\xFF\xFF')
-        # self.transport.write('\x06\x88\x34\xFF\xFF\xFF\xFF')
-
-    timeout = 0.3
+        self.data = self.data + data
 
     def call(self, req):
         return self.lock.run(self._call, req)
 
-    @defer.inlineCallbacks
     def _call(self, req):
+        self.data = ''
         d = defer.Deferred()
-        self.defer = d
         if self.req:
             raise ValueError(
-                "call %s while %s request in progress" % (req, self.req))
+                "call %s while %s request in progress" % (
+                    pretty(req), pretty(self.req)))
         self.req = req
 
-        timedefer = reactor.callLater(
-            self.timeout, lambda d: d.errback(
-                ValueError("No answer after %ss" % self.timeout)), d)
-        # packet = self.dec.encode(req)
+        def timeout(d):
+            if self.data:
+                self.req = None
+                d.callback(self.data)
+                self.data = ''
+            else:
+                logger.debug("Timeout")
         self.transport.write(req)
-        try:
-            results = yield self.defer
-        except Exception as e:
-            print e
+        reactor.callLater(self.timeout, timeout, d)
+        return d
 
-        timedefer.cancel()
-        self.req = None
-        self.data = ''
-        defer.returnValue(results)
-
-    def charger_init(self):
-        print 'send init'
+    @log_result
+    def changer_init(self):
         return self.call('\x02\x85\x0A')
-
-    def mdb_reset(self):
-        print 'send reset'
-        return self.call('\x02\x88\x08')
-
-    @defer.inlineCallbacks
-    def charger_poll(self):
-        # print "start poll"
-        result = yield self.call('\x02\x88\x0B')
-        print "Polling: {}".format(result)
-        # print "end poll"
-        defer.returnValue(result)
-
-    def charger_payin(self):
-        return self.call('\x06\x88\x0C\xFF\xFF\xFF\xFF')
-
 
     @defer.inlineCallbacks
     def work(self):
-        result = yield self.charger_init()
-        print "init result: {}".format(result)
-        # result = yield self.mdb_reset()
-        # print "reset result: {}".format(result)
-
-        # loop = task.LoopingCall(self.charger_poll)
-        # loop.start(1.5)
+        yield self.charger_init()
+        yield self.mdb_reset()
         while True:
-            yield self.charger_poll()
+            try:
+                yield self.charger_poll()
+            except Exception:
+                logger.exception('Error while polling')
 
 
-        # result = yield self.charger_poll()
-        # print "poll result: {}".format(result)
+class Kiosk(object):
 
-        # result = yield self.charger_payin()
-        # print "payin result: {}".format(result)
+    def __init__(self, proto):
+        self.proto = proto
+        self.changer = Changer(proto)
+        self.bill = BillValidator(proto)
 
+    @defer.inlineCallbacks
+    def loop(self):
+        yield self.proto.changer_init()
+        yield self.changer.reset()
+        # yield self.bill.reset()
+        # yield self.bill.bill_type()
+        # yield self.bill.escrow()
+        while True:
+            try:
+                yield self.changer.poll()
+                # yield self.bill.poll()
+            except Exception:
+                logger.exception('Error while polling')
 
-
-
-proto = MDB()
-SerialPort(
-    proto, '/dev/ttyUSB0', reactor,
-    baudrate='38400', parity=PARITY_NONE,
-    bytesize=EIGHTBITS, stopbits=STOPBITS_ONE)
-reactor.callLater(1, proto.work)
-reactor.callLater(3, proto.charger_payin)
-# reactor.callLater(4, proto.payout)
-print "run reactor"
-reactor.run()
-
-
+if __name__ == '__main__':
+    proto = MDB()
+    SerialPort(
+        proto, '/dev/ttyUSB0', reactor,
+        baudrate='38400', parity=PARITY_NONE,
+        bytesize=EIGHTBITS, stopbits=STOPBITS_ONE)
+    kiosk = Kiosk(proto)
+    reactor.callLater(1, kiosk.loop)
+    logger.debug("run reactor")
+    reactor.run()
